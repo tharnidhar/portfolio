458129c5019f21561288d773e374b21fdaca8c4c
Tharnidhar <tharnidhar.csgrad@gmail.com>
2024-08-17 18:24:23 +0530

    Typewriter added

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-alert("this is working");
-function hamburg() {}
+function hamburg() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(0px)";
+}
+function cancel() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(-500px)";
+}
+
+const texts = ["DESIGN", "DEVELOP APPLICATIONS", "PHOTOGRAPH", "EDIT"];
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter_text");
+
+let textIndex = 0;
+let characterIndex = 0;
+
+function typeWriter() {
+  if (characterIndex < texts[textIndex].length) {
+    textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+    characterIndex++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(eraseText, 1000);
+  }
+}
+window.onload = typeWriter;
+
+function eraseText() {
+  if (textElements.innerHTML.length > 0) {
+    textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+    setTimeout(eraseText, 50);
+  } else {
+    textIndex = (textIndex + 1) % texts.length;
+    characterIndex = 0;
+    setTimeout(typeWriter, 500);
+  }
+}
